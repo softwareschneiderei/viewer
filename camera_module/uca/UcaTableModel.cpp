d04664dc475dd33e24ac643c7ac86445e97b3894
Marius Elvert <marius.elvert@softwareschneiderei.de>
2017-08-15 16:55:16 +0200

    Added some very simple uca device property editing

--- camera_module/uca/UcaTableModel.cpp
+++ camera_module/uca/UcaTableModel.cpp
@@ -1,4 +1,5 @@
 #include "UcaTableModel.h"
+#include <QDebug>
 
 QModelIndex UcaTableModel::parent(const QModelIndex &child) const
 {
@@ -18,17 +19,19 @@ int UcaTableModel::columnCount(const QModelIndex &parent) const
 QVariant UcaTableModel::data(const QModelIndex &index, int role) const
 {
   if (role != Qt::DisplayRole)
+  {
     return {};
+  }
 
   auto parameterSpec = mProperties[index.row()];
-  if (index.column()==0)
+  if (index.column() == 0)
   {
     QString name = g_param_spec_get_name(parameterSpec);
     return name;
   }
-  else if (index.column()==1)
+  else if (index.column() == 1)
   {
-    GValue value={};
+    GValue value = {};
     g_value_init(&value, G_PARAM_SPEC_VALUE_TYPE(parameterSpec));
 
     g_object_get_property(G_OBJECT(mCamera), g_param_spec_get_name(parameterSpec), &value);
@@ -43,12 +46,12 @@ QVariant UcaTableModel::data(const QModelIndex &index, int role) const
 }
 
 UcaTableModel::UcaTableModel(UcaCamera* camera)
-: mCamera(camera)
+  : mCamera(camera)
 {
   guint propertyCount = 0;
   auto properties = g_object_class_list_properties(G_OBJECT_GET_CLASS(camera), &propertyCount);
 
-  for (guint i=0; i < propertyCount; ++i)
+  for (guint i = 0; i < propertyCount; ++i)
   {
     mProperties.push_back(properties[i]);
   }
@@ -60,17 +63,86 @@ UcaTableModel::~UcaTableModel()
 {
 
 
-
 }
 
 QVariant UcaTableModel::headerData(int section, Qt::Orientation orientation, int role) const
 {
   if (orientation != Qt::Horizontal || role != Qt::DisplayRole)
+  {
     return QAbstractItemModel::headerData(section, orientation, role);
+  }
 
   if (section == 0)
+  {
     return QString("Property");
+  }
   if (section == 1)
+  {
     return QString("Value");
+  }
   return {};
+}
+
+bool UcaTableModel::setData(const QModelIndex &index, QVariant const &newValue, int role)
+{
+  if (index.isValid() && role == Qt::EditRole)
+  {
+    int row = index.row();
+
+    if (index.column() != 1)
+    {
+      return false;
+    }
+
+    qDebug() << "Edit: " << newValue;
+    auto property = mProperties[index.row()];
+    bool ok = true;
+
+    GValue value = {0};
+    g_value_init(&value, property->value_type);
+    switch (property->value_type)
+    {
+    case G_TYPE_INT:
+      g_value_set_int(&value, newValue.toInt(&ok));
+      break;
+    case G_TYPE_UINT:
+      g_value_set_uint(&value, newValue.toUInt(&ok));
+      break;
+    case G_TYPE_DOUBLE:
+      g_value_set_double(&value, newValue.toDouble(&ok));
+      break;
+    default:
+      ok = false;
+      break;
+    };
+
+    if (ok)
+    {
+      g_object_set_property(G_OBJECT (mCamera), property->name, &value);
+      emit(dataChanged(index, index));
+
+      qDebug() << "Edited value";
+    }
+    else
+    {
+      qDebug() << "Did not edit value";
+    }
+
+    g_value_unset(&value);
+
+    return true;
+  }
+
+  return false;
+}
+
+Qt::ItemFlags UcaTableModel::flags(QModelIndex const &index) const
+{
+  auto property = mProperties[index.row()];
+  if (property && property->flags & G_PARAM_WRITABLE)
+  {
+    return QAbstractTableModel::flags(index) | Qt::ItemIsEditable;
+  }
+
+  return QAbstractTableModel::flags(index);
 }
--- camera_module/uca/UcaTableModel.h
+++ camera_module/uca/UcaTableModel.h
@@ -5,8 +5,8 @@
 class UcaTableModel : public  QAbstractTableModel
 {
 public:
-  UcaTableModel(UcaCamera* camera);
-  ~UcaTableModel();
+  explicit UcaTableModel(UcaCamera* camera);
+  ~UcaTableModel() override;
 
   int rowCount(const QModelIndex &parent) const override;
   int columnCount(const QModelIndex &parent) const override;
@@ -15,8 +15,9 @@ public:
   QVariant data(const QModelIndex &index, int role) const override;
 
   QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
+  bool setData(const QModelIndex &index, const QVariant &value, int role);
 
-
+  Qt::ItemFlags flags(QModelIndex const& index) const override;
 private:
   UcaCamera* mCamera;
   std::vector<GParamSpec*> mProperties;
